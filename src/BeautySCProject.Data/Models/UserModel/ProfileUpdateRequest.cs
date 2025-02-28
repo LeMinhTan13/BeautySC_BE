14ee665725867718ce28a3172fe5647ef90dd06c
zodicto <minhtan210703@gmail.com>
2025-02-28 09:49:01 +0700

    update api

--- src/BeautySCProject.Data/Models/UserModel/ProfileUpdateRequest.cs
+++ src/BeautySCProject.Data/Models/UserModel/ProfileUpdateRequest.cs
@@ -8,10 +8,8 @@ namespace BeautySCProject.Data.Models.CustomerModel
     {
         [MaxLength(255)]
         public string? FullName { get; set; }
-        [RegularExpression(@"^\+(?:[0-9]?){6,14}[0-9]$", ErrorMessage = "Must be a E.164 compliant phone number (ex: +84332338587)")]
         public string? PhoneNumber { get; set; }
 
-        [BirthdayValidation(18, 100, ErrorMessage = "Date of birth out of range")]
         public DateOnly? Birthday { get; set; }      
     } 
 }
