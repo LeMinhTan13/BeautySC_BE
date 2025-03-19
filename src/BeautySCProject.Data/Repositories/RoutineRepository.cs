61332c7197bdd081c3239573e64f3bc62ccd56e5
ThanhNTTFPT <ThanhNTSE170542@fpt.edu.vn>
2025-03-20 04:00:50 +0700

    merge

--- src/BeautySCProject.Data/Repositories/RoutineRepository.cs
+++ src/BeautySCProject.Data/Repositories/RoutineRepository.cs
@@ -49,13 +49,13 @@ namespace BeautySCProject.Data.Repositories
         {
             try
             {
-                if (routine.Status == true)
+                var existingRoutines = await Entities
+                    .Where(x => x.SkinTypeId == routine.SkinTypeId && x.Status == true)
+                    .ToListAsync();
+
+                foreach (var item in existingRoutines)
                 {
-                    var routines = await Entities.Where(x => x.Status == true).ToListAsync();
-                    foreach (var item in routines)
-                    {
-                        item.Status = false;
-                    }
+                    item.Status = false;
                 }
                 Entities.Add(routine);
                 await _unitOfWork.SaveChangesAsync();
@@ -71,13 +71,13 @@ namespace BeautySCProject.Data.Repositories
         {
             try
             {
-                if (routine.Status == true)
+                var existingRoutines = await Entities
+                    .Where(x => x.SkinTypeId == routine.SkinTypeId && x.Status == true)
+                    .ToListAsync();
+
+                foreach (var item in existingRoutines)
                 {
-                    var routines = await Entities.Where(x => x.Status == true).ToListAsync();
-                    foreach (var item in routines)
-                    {
-                        item.Status = false;
-                    }
+                    item.Status = false;
                 }
                 Entities.Update(routine);
                 await _unitOfWork.SaveChangesAsync();
@@ -106,43 +106,21 @@ namespace BeautySCProject.Data.Repositories
 
         public async Task<Routine> GetRoutinesByRoutineIdAsync(int routineId)
         {
-            var routine = await Entities
-                .Where(r => r.RoutineId == routineId)
-                .Include(r => r.SkinType)
-                .Include(r => r.RoutineDetails)
-                    .ThenInclude(rd => rd.RoutineSteps)
-                        .ThenInclude(rs => rs.Category)
-                            .ThenInclude(c => c.Products)
-                                .ThenInclude(p => p.ProductSkinTypes) // Include danh sách ProductSkinTypes
-                .Include(r => r.RoutineDetails)
-                    .ThenInclude(rd => rd.RoutineSteps)
-                        .ThenInclude(rs => rs.Category)
-                            .ThenInclude(c => c.Products)
-                                .ThenInclude(p => p.ProductImages) // Include ảnh sản phẩm
-                .Include(r => r.RoutineDetails)
-                    .ThenInclude(rd => rd.RoutineSteps)
-                        .ThenInclude(rs => rs.Category)
-                            .ThenInclude(c => c.Products)
-                                .ThenInclude(p => p.ProductIngredients) // Include danh sách ProductIngredients
-                                    .ThenInclude(pi => pi.Ingredient) // Include thông tin Ingredient của sản phẩm
-                .FirstOrDefaultAsync();
+            return await Entities
+              .Where(r => r.RoutineId == routineId)
+              .Include(r => r.SkinType)
+              .Include(r => r.RoutineDetails)
+                  .ThenInclude(rd => rd.RoutineSteps)
+                      .ThenInclude(rs => rs.Category)
+                          .ThenInclude(c => c.Products)
+                                      .ThenInclude(p => p.ProductImages)
+              .Include(r => r.RoutineDetails).ThenInclude(rd => rd.RoutineSteps)
+                         .ThenInclude(rs => rs.Category)
+                             .ThenInclude(c => c.Products)
+                .ThenInclude(p => p.ProductIngredients)
+                                     .ThenInclude(pi => pi.Ingredient)
+             .FirstOrDefaultAsync();
 
-
-            // Lọc danh sách sản phẩm sau khi lấy từ DB
-            if (routine != null)
-            {
-                foreach (var routineDetail in routine.RoutineDetails)
-                {
-                    foreach (var routineStep in routineDetail.RoutineSteps)
-                    {
-                        routineStep.Category.Products = routineStep.Category.Products
-                            .Where(p => p.ProductSkinTypes.Any(pst => pst.SkinTypeId == routine.SkinType.SkinTypeId))
-                            .ToList();
-                    }
-                }
-            }
-
-            return routine;
         }
 
         public async Task<Routine> GetRoutinesByRoutineIdForAminAsync(int routineId)
